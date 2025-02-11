2000170f528169b424780b61ab7832a5eee8dee5
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-11 23:29:48 +0300

    Ajout de nouveaux composants GridPattern et Button, mise à jour des chemins d'importation et ajout de dépendances

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,36 @@
+import { cn } from "@/lib/utils"
+import { GridPattern } from "@/src/components/magicui/grid-pattern"
+
+
 export default function Home() {
-    /**
-     * ! STATE (état, données) de l'application
-     */
-    
-    
-    /**
-     * ! COMPORTEMENT (méthodes, fonctions) de l'application
-     */
-    
-    
-    /**
-     * ! AFFICHAGE (render) de l'application
-     */
-    return (
-        <>
-            <h1>Mon application</h1>
-            <p>Bienvenue sur mon application</p>
-        </>
-    )
+  /**
+   * ! STATE (état, données) de l'application
+   */
+
+
+  /**
+   * ! COMPORTEMENT (méthodes, fonctions) de l'application
+   */
+
+
+  /**
+   * ! AFFICHAGE (render) de l'application
+   */
+  return (
+    <div className="flex min-h-screen flex-col">
+      <GridPattern width={50} height={50} x={-1} y={-1} strokeDasharray={"4 2"} className={cn("absolute inset-0 [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]")} />
+      {/* En-tête */}
+      <header>
+        {/* <Navbar /> */}
+      </header>
+
+      {/* Contenu principal */}
+      <main className="grow">
+
+      </main>
+
+      {/* Pied de page */}
+      <footer></footer>
+    </div >
+  )
 }
