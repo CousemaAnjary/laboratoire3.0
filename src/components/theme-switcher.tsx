77346b3634a9f7ee8edba381d3d8731424d55c78
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-12 00:07:42 +0300

    Refactor du composant ThemeSwitcher pour utiliser des boutons personnalisés et améliorer l'interface utilisateur

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { Button } from "./ui/button"
 import { useTheme } from "next-themes"
 import { Monitor, MoonStar, Sun } from "lucide-react"
 
@@ -19,22 +20,16 @@ export default function ThemeSwitcher() {
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <div className="flex items-center space-x-2 rounded-full border p-1 px-2">
-            <Sun
-                className={`cursor-pointer transition-all duration-300 ${theme === "light" ? "rounded-full border p-1" : "size-4"
-                    }`}
-                onClick={() => setTheme("light")}
-            />
-            <Monitor
-                className={`cursor-pointer transition-all duration-300 ${theme === "system" ? "rounded-full border p-1" : "size-4"
-                    }`}
-                onClick={() => setTheme("system")}
-            />
-            <MoonStar
-                className={`cursor-pointer transition-all duration-300 ${theme === "dark" ? "rounded-full border p-1" : "size-4"
-                    }`}
-                onClick={() => setTheme("dark")}
-            />
+        <div className="flex gap-1">
+            <Button variant={theme === "light" ? "outline" : "ghost"} size="icon" className="size-8" onClick={() => setTheme("light")}>
+                <Sun className="size-4" />
+            </Button>
+            <Button variant={theme === "system" ? "outline" : "ghost"} size="icon" className="size-8" onClick={() => setTheme("system")}>
+                <Monitor className="size-4" />
+            </Button>
+            <Button variant={theme === "dark" ? "outline" : "ghost"} size="icon" className="size-8" onClick={() => setTheme("dark")}>
+                <MoonStar className="size-4" />
+            </Button>
         </div>
     )
 }
