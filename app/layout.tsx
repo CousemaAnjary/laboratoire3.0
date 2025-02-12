7c3db344dfe7266ef812d75f60772bc486133a6b
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-13 00:26:03 +0300

    Ajout d'un wrapper SessionProvider pour gérer les sessions d'authentification

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/src/components/theme-provider";
+import SessionProviderWrapper from "@/src/components/session-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,8 +35,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-
-          {children}
+          <SessionProviderWrapper>
+            {children}
+          </SessionProviderWrapper>
 
         </ThemeProvider>
 
