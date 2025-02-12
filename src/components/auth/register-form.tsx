6d6d6a959cbb308a786e1221f7a3c021df1d5656
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-12 21:41:05 +0300

    Ajout du schéma de validation pour le formulaire d'inscription et création du composant RegisterForm

---
+++ src/components/auth/register-form.tsx
@@ -0,0 +1,166 @@
+"use client"
+import { z } from "zod"
+import Link from "next/link"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { Input } from "@/src/components/ui/input"
+import { Eye, EyeOff, Loader } from "lucide-react"
+import { Button } from "@/src/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { RegisterSchema } from "@/src/lib/schemas/auth"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/src/components/ui/form"
+
+
+export default function RegisterForm() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+    const [loading, setLoading] = useState(false)
+    const [showPassword, setShowPassword] = useState(false)
+
+    const form = useForm<z.infer<typeof RegisterSchema>>({
+        resolver: zodResolver(RegisterSchema),
+        defaultValues: {
+            lastname: "",
+            firstname: "",
+            email: "",
+            password: "",
+        }
+    })
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+    const handleRegister = async (data: z.infer<typeof RegisterSchema>) => {
+        console.log(data)
+        setLoading(true)
+    }
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+            <div className="relative w-full max-w-md p-8">
+                <h1 className="mb-2 font-spaceGrotesk text-2xl font-medium">Inscription</h1>
+                <p className="mb-4 font-spaceGrotesk text-sm font-medium text-muted-foreground">
+                    Vous avez déjà un compte ? Accédez-y en cliquant <Link href="/auth/login" className="text-cyan-700 underline">ici</Link>
+                </p>
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleRegister)}>
+                        <div className="grid gap-4">
+
+                            <div className="grid grid-cols-2 gap-2">
+                                <div className="grid gap-2">
+                                    <FormField
+                                        control={form.control}
+                                        name="lastname"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel className="font-inter">Nom</FormLabel>
+                                                <FormControl>
+                                                    <Input {...field} placeholder="ABDILLAH" className="bg-white font-inter shadow-sm dark:bg-zinc-950" />
+                                                </FormControl>
+                                                <FormMessage className="font-inter" />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <FormField
+                                        control={form.control}
+                                        name="firstname"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel className="font-inter">Prénom</FormLabel>
+                                                <FormControl>
+                                                    <Input {...field} placeholder="Cousema Anjary" className="bg-white font-inter shadow-sm dark:bg-zinc-950" />
+                                                </FormControl>
+                                                <FormMessage className="font-inter" />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel className="font-inter">Adresse email</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} placeholder="exemple@gmail.com" className="bg-white font-inter shadow-sm dark:bg-zinc-950" />
+                                            </FormControl>
+                                            <FormMessage className="font-inter" />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+
+                            <div className="grid grid-cols-9 gap-2">
+                                <div className="col-span-8 grid gap-2">
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel className="font-inter">Mot de passe</FormLabel>
+                                                <FormControl>
+                                                    <Input {...field} type={showPassword ? "text" : "password"} placeholder="Entrez votre mot de passe" className="bg-white font-inter shadow-sm dark:bg-zinc-950" />
+                                                </FormControl>
+                                                <FormMessage className="font-inter" />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <div className="mb-1 grid">
+                                    <Button type="button" variant="outline" size={"icon"} className="mt-8" onClick={() => setShowPassword(!showPassword)}>
+                                        {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+                                    </Button>
+                                </div>
+                            </div>
+
+                            {/* <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="image"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel className="font-inter">Image</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    type="file"
+                                                    accept="image/*"
+                                                    className="shadow-sm bg-white font-inter text-xs"
+                                                    onChange={(e) => { field.onChange(e.target.files ? e.target.files[0] : null) }}
+                                                />
+                                            </FormControl>
+                                            <FormMessage className="font-inter" />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div> */}
+
+                            <div className="grid">
+                                <Button type="submit" className="w-full font-spaceGrotesk font-medium" disabled={loading}>
+                                    {loading ? (
+                                        <>
+                                            <Loader className="mr-2 size-4 animate-spin" />
+                                            Veuillez patienter
+                                        </>
+                                    ) : (
+                                        "Créer un compte"
+                                    )}
+                                </Button>
+                            </div>
+                        </div>
+                    </form>
+
+                </Form>
+            </div>
+        </>
+    )
+}
--- src/lib/schemas/auth.ts
+++ src/lib/schemas/auth.ts
@@ -4,3 +4,27 @@ export const LoginSchema = z.object({
     email: z.string().email("L'adresse email est invalide"),
     password: z.string().min(6, "Le mot de passe doit contenir au moins 6 caractères"),
 })
+
+export const RegisterSchema = z.object({
+    lastname: z.string().nonempty("Le nom est obligatoire"),
+    firstname: z.string().nonempty("Le prénom est obligatoire"),
+    email: z.string().email("L'adresse email est invalide"),
+    password: z.string().min(8, "Le mot de passe doit contenir au moins 8 caractères"),
+    // image: z
+    //     // Vérifie que l’entrée est bien un fichier (File).
+    //     .custom<File>((value) => value instanceof File, {
+    //         message: "Le fichier doit être un fichier valide.",
+    //     })
+
+    //     //Vérifie que le fichier est une image (par exemple, image/png, image/jpeg)
+    //     .refine((file) => file?.type.startsWith("image/"), {
+    //         message: "Le fichier doit être une image.",
+    //     })
+
+    //     //Vérifie que la taille du fichier est inférieure à 5 Mo
+    //     // .refine((file) => file?.size < 5 * 1024 * 1024, {
+    //     //     message: "Le fichier doit être inférieur à 5 Mo.",
+    //     // }),
+
+    //     .optional()
+})
