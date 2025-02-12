4462f34877556e743777796b4a327943cef43eba
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-13 01:34:41 +0300

    Ajout de l'ID utilisateur dans la session lors de l'authentification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,7 +9,12 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
     callbacks: {
-       
+        async session({ session, token }) {
+            if (token.sub) {
+                session.user.id = token.sub; // ✅ Ajoute l'ID utilisateur dans la session
+            }
+            return session;
+        },
     },
     pages: {
         signIn: "/login",
