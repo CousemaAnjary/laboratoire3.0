17ea2ead1b051a1b21a29d04b15ce5199ae5a52c
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-13 00:34:07 +0300

    Ajout de l'affichage du nom de l'utilisateur dans le tableau de bord après la récupération de la session

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,20 +1,29 @@
+"use client"
+
+import { signIn, useSession } from "next-auth/react";
+
 export default function Dashboard() {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+    const { data: session } = useSession()
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
         <>
             <h1>Dashboard</h1>
+            <p>Welcome {session?.user?.name}</p>
+
+            <button onClick={() => signIn("github", { redirectTo: "/dashboard" })}>
+                Sign In
+            </button>
         </>
     )
 }
