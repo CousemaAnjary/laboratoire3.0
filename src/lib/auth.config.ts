df7345b666cc184bc011ab7b48b5f9e12259ddd3
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-13 17:56:23 +0300

    Refactor la gestion des erreurs lors de l'authentification et suppression du composant AnimatedInput

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -17,30 +17,40 @@ export default {
 
             authorize: async (credentials) => {
 
-                // Validation des données reçues via votre schéma(par exemple avec Zod)
-                const validated = LoginSchema.safeParse(credentials)
+                try {
+                    // Validation des données reçues via votre schéma(par exemple avec Zod)
+                    const validated = LoginSchema.safeParse(credentials)
 
-                // Retourner une erreur si les données ne sont pas valides
-                if (!validated.success) {
-                    throw new Error("Données invalides. Vérifiez votre saisie.")
+                    // Retourner une erreur si les données ne sont pas valides
+                    if (!validated.success) {
+                        throw new Error("Données invalides. Vérifiez votre saisie.")
+                    }
+
+                    // Vérifier si l'utilisateur existe dans la base de données
+                    const user = await getUserByEmail(validated.data.email)
+
+                    if (!user) {
+                        throw new Error("Aucun compte trouvé avec cet e-mail.")
+                    }
+
+                    // Vérifier le mot de passe
+                    const isPasswordValid = await bcrypt.compare(validated.data.password, user.password || "")
+
+                    if (!isPasswordValid) {
+                        throw new Error("Mot de passe incorrect.")
+                    }
+
+                    // Retourner l'utilisateur pour créer une session
+                    return user
+                    
+                } catch (error) {
+                    // Retourner une erreur si quelque chose ne va pas
+                    if (error instanceof Error) {
+                        throw new Error(error.message);
+                    } else {
+                        throw new Error("Une erreur inconnue s'est produite.");
+                    }
                 }
-
-                // Vérifier si l'utilisateur existe dans la base de données
-                const user = await getUserByEmail(validated.data.email)
-
-                if (!user) {
-                    throw new Error("Aucun compte trouvé avec cet e-mail.")
-                }
-
-                // Vérifier le mot de passe
-                const isPasswordValid = await bcrypt.compare(validated.data.password, user.password || "")
-
-                if (!isPasswordValid) {
-                    throw new Error("Mot de passe incorrect.")
-                }
-
-                // Retourner l'utilisateur pour créer une session
-                return user
             },
         })
 
