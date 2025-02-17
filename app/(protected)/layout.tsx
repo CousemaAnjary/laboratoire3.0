053bfc3bcc01e13af46986170748cce72690520f
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-17 09:53:08 +0300

    Ajout de la barre latérale d'application avec SidebarProvider et SidebarInset dans le layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,19 +1,30 @@
-export default function layout() {
+import AppSidebar from "@/src/components/admin-panel/app-sidebar"
+import { SidebarInset, SidebarProvider } from "@/src/components/ui/sidebar"
+
+
+export default function layout({ children }: { children: React.ReactNode }) {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <>
-        </>
+        <SidebarProvider className="bg-slate-100">
+            <AppSidebar />
+            <SidebarInset className="relative h-screen overflow-x-hidden bg-slate-100"> {/* Ajoutez overflow-x-hidden */}
+                {/* <Navbar /> */}
+                <main className="container py-8 sm:px-8"> {/* Ajoutez overflow-x-auto */}
+                    {children}
+                </main>
+            </SidebarInset>
+        </SidebarProvider>
     )
 }
