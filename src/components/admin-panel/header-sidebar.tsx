7d3ae5d341ef909698bef25eb0d276251fc7ed13
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-17 11:10:24 +0300

    Augmentation de la taille de l'icône CodeXml dans le composant HeaderSidebar

--- src/components/admin-panel/header-sidebar.tsx
+++ src/components/admin-panel/header-sidebar.tsx
@@ -1,5 +1,4 @@
 import { CodeXml } from "lucide-react"
-
 import { SidebarMenuButton } from "../ui/sidebar"
 
 
@@ -20,7 +19,7 @@ export default function HeaderSidebar() {
     return (
         <SidebarMenuButton size="lg">
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-blue-900 text-sidebar-primary-foreground hover:bg-blue-900">
-                <CodeXml className="size-4" />
+                <CodeXml className="size-5" />
             </div>
 
             <div className="grid flex-1 text-left text-sm leading-tight">
