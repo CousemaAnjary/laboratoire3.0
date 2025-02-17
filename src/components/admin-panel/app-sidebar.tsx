107dacc3a5a3d7b24713819908218e282e73de2d
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-17 09:47:53 +0300

    Ajout de nouveaux composants UI : ScrollArea, Separator, Tooltip et Skeleton, ainsi que l'utilisation de la dépendance @radix-ui/react-scroll-area dans package.json.

---
+++ src/components/admin-panel/app-sidebar.tsx
@@ -0,0 +1,38 @@
+import { ScrollArea } from "../ui/scroll-area"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarSeparator } from "../ui/sidebar"
+
+export default function AppSidebar() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <Sidebar variant="floating" collapsible="icon">
+            {/* En-tête de la barre latérale */}
+            <SidebarHeader>
+                {/* <HeaderSidebar /> */}
+            </SidebarHeader>
+
+            <SidebarSeparator />
+
+            {/* Contenu de la barre latérale */}
+            <ScrollArea>
+                <SidebarContent>
+                    {/* <SidebarContentMenu menuGroups={menuGroups} /> */}
+                </SidebarContent>
+            </ScrollArea>
+
+            {/* Pied de page de la barre latérale */}
+            <SidebarFooter />
+        </Sidebar>
+    )
+}
