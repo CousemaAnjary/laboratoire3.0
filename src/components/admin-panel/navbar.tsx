82411f288702933e322c8e0dc847ab32b44fbb8a
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-17 10:01:46 +0300

    Ajout du composant Navbar et du menu déroulant utilisateur, ainsi que l'intégration de la dépendance @radix-ui/react-avatar dans package.json et package-lock.json.

---
+++ src/components/admin-panel/navbar.tsx
@@ -0,0 +1,31 @@
+import { Separator } from "../ui/separator"
+import { SidebarTrigger } from "../ui/sidebar"
+import UserDropdownMenu from "./user-dropdown-menu"
+
+export default function Navbar() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <nav className="sticky top-0 z-50 mx-auto flex items-center justify-between py-4 backdrop-blur-lg">
+            <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                {/* <DynamicBreadcrumb /> */}
+            </div>
+            <div className="me-10 flex justify-end space-x-3">
+                <UserDropdownMenu />
+            </div>
+        </nav>
+    )
+}
---
+++ src/components/admin-panel/user-dropdown-menu.tsx
@@ -0,0 +1,64 @@
+import { LogOut, Settings, User } from "lucide-react";
+import Link from "next/link";
+import { AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+
+export default function UserDropdownMenu() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant="outline"
+                    className="relative size-8 rounded-full"
+                >
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+                <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                        <p className="mb-1 font-inter text-sm font-medium leading-none"> ABDILLAH Cousema Anjary</p>
+                        <p className="font-inter text-xs leading-none text-muted-foreground">
+                            {/* {user?.email} */}
+                        </p>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem className="hover:cursor-pointer" asChild>
+                        <Link href="#" className="flex items-center font-inter">
+                            <User className="mr-3 size-4 text-muted-foreground" />
+                            Profile
+                        </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="hover:cursor-pointer" asChild>
+                        <Link href="#" className="flex items-center font-inter">
+                            <Settings className="mr-3 size-4 text-muted-foreground" />
+                            Paramètres
+                        </Link>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="font-inter hover:cursor-pointer">
+                    <LogOut className="mr-3 size-4 text-red-600" />
+                    Déconnexion
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
