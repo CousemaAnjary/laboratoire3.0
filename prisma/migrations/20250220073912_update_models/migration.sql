084c3af322277f6558d9c21cf4caf002045c5f4a
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-20 10:40:49 +0300

    Ajout des modèles de facturation et des lignes de factures dans le fichier schema.prisma, ainsi que la création des types et tables correspondants dans la migration SQL.

---
+++ prisma/migrations/20250220073912_update_models/migration.sql
@@ -0,0 +1,43 @@
+-- CreateEnum
+CREATE TYPE "InvoiceStatus" AS ENUM ('PENDING', 'PAID', 'OVERDUE', 'CANCELLED');
+
+-- CreateTable
+CREATE TABLE "Invoice" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "issuerName" TEXT NOT NULL DEFAULT '',
+    "issuerAddress" TEXT NOT NULL DEFAULT '',
+    "clientName" TEXT NOT NULL DEFAULT '',
+    "clientAddress" TEXT NOT NULL DEFAULT '',
+    "invoiceDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "vatActive" BOOLEAN NOT NULL DEFAULT false,
+    "vatRate" DOUBLE PRECISION NOT NULL DEFAULT 20,
+    "status" "InvoiceStatus" NOT NULL DEFAULT 'PENDING',
+    "totalAmount" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "InvoiceLine" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT '',
+    "quantity" INTEGER NOT NULL DEFAULT 1,
+    "unitPrice" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "totalPrice" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "invoiceId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "InvoiceLine_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Invoice" ADD CONSTRAINT "Invoice_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "InvoiceLine" ADD CONSTRAINT "InvoiceLine_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "Invoice"("id") ON DELETE CASCADE ON UPDATE CASCADE;
