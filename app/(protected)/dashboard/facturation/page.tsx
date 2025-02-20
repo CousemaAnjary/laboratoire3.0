34ed3adb1440ff8be720fd5dcd279c56a097b389
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-20 11:38:10 +0300

    Amélioration de la largeur de la carte de création de facture pour une meilleure présentation dans le composant Facturation.

--- app/(protected)/dashboard/facturation/page.tsx
+++ app/(protected)/dashboard/facturation/page.tsx
@@ -16,12 +16,12 @@ export default function Facturation() {
      */
     return (
         <>
-            <div className="mb-8  flex w-full items-center justify-between rounded-md bg-white p-4 shadow-sm">
+            <div className="mb-8 flex w-full items-center justify-between rounded-md bg-white p-4 shadow-sm">
                 <h1 className="font-spaceGrotesk font-medium text-gray-800">Mes factures en cours</h1>
             </div>
 
             <div className="grid cursor-pointer gap-4 md:grid-cols-3">
-                <Card className="rounded-sm  border-dashed border-slate-300 bg-transparent shadow-sm">
+                <Card className="w-[350px] rounded-sm  border-dashed border-slate-300 bg-transparent shadow-sm">
                     <CardHeader>
                         <CardTitle className="font-inter font-medium">Créer une facture</CardTitle>
                         <CardDescription className="font-spaceGrotesk">Créer une nouvelle facture pour un client</CardDescription>
