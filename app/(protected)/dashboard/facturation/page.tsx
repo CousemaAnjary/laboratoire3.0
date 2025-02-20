dcc08d5f71fbe3de8c3518649d39251051c84c61
CousemaAnjary <anjaryabdillah@gmail.com>
2025-02-20 11:25:55 +0300

    Amélioration de la mise en page de la page de facturation en ajustant la largeur des cartes et en modifiant la classe CSS pour une meilleure présentation.

--- app/(protected)/dashboard/facturation/page.tsx
+++ app/(protected)/dashboard/facturation/page.tsx
@@ -16,12 +16,12 @@ export default function Facturation() {
      */
     return (
         <>
-            <div className="mb-8 flex items-center justify-between rounded-md bg-white p-4 shadow-sm">
+            <div className="mb-8  flex w-full items-center justify-between rounded-md bg-white p-4 shadow-sm">
                 <h1 className="font-spaceGrotesk font-medium text-gray-800">Mes factures en cours</h1>
             </div>
 
             <div className="grid cursor-pointer gap-4 md:grid-cols-3">
-                <Card className="rounded-sm shadow-sm">
+                <Card className="w-[350px] rounded-sm shadow-sm">
                     <CardHeader>
                         <CardTitle className="font-inter font-medium">Créer une facture</CardTitle>
                         <CardDescription className="font-spaceGrotesk">Créer une nouvelle facture pour un client</CardDescription>
