95addaeac26a919d8e920c70976d3b8610211636
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-14 07:40:35 +0300

    Suppression des anciennes migrations et des fichiers d'authentification, ajout de nouvelles tables et configuration de l'authentification avec better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,19 @@ import { prismaAdapter } from "better-auth/adapters/prisma"
 
 
 export const auth = betterAuth({
+    // Use the Prisma adapter to store user data
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+
+    emailAndPassword: {
+        enabled: true
+    },
+
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }
+    },
 });
