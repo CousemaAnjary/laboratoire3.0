147bd8431b551b7de19edb93e41100d560568a1c
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-14 21:37:31 +0300

    Ajout de la prise en charge des images pour les domaines Google et GitHub dans la configuration Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "lh3.googleusercontent.com", // Google OAuth
+      "avatars.githubusercontent.com", // GitHub OAuth
+    ],
+  },
 };
 
 export default nextConfig;
