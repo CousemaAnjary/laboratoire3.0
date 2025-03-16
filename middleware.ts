86fa37ad73e52bcf15b735f408290361a0c39a16
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-16 19:54:37 +0300

    Ajout de la gestion des accès aux pages de vérification d'email et de réinitialisation de mot de passe dans le middleware.

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { getSessionCookie } from "better-auth/cookies"
 // 🔹 Définition des routes protégées et publiques
 const protectedRoutes = ["/dashboard"]
 const publicRoutes = ["/login", "/register", "/"]
+const restrictedForAuthenticated = ["/email-verified", "/reset-password"];
 
 
 export async function middleware(request: NextRequest) {
@@ -17,17 +18,41 @@ export async function middleware(request: NextRequest) {
     })
 
     // Verification des routes protégées et publiques
-    const isProtectedRoute = protectedRoutes.some(route => url.pathname.startsWith(route))
     const isPublicRoute = publicRoutes.includes(url.pathname)
+    const isRestrictedRoute = restrictedForAuthenticated.includes(url.pathname)
+    const isProtectedRoute = protectedRoutes.some(route => url.pathname.startsWith(route))
+
+
 
     // 🔹 Redirection si l'utilisateur **n'est pas connecté** et tente d'accéder à une **page protégée**
     if (isProtectedRoute && !sessionCookie) {
         return NextResponse.redirect(new URL("/login", request.url))
     }
 
-    // 🔹 Redirection si l'utilisateur **est connecté** et tente d'accéder à une **page publique**
-    if (isPublicRoute && sessionCookie) {
+    // 🔹 Redirection si l'utilisateur **est connecté** et tente d'accéder à une **page publique** et **page restreinte**
+    if ((isPublicRoute || isRestrictedRoute) && sessionCookie) {
         return NextResponse.redirect(new URL("/dashboard", request.url))
     }
+
+    // 🔹 Vérification des accès aux pages sensibles (email verification & reset password)
+    if (url.pathname === "/email-verified") {
+        const email = request.cookies.get("emailToVerify")?.value
+
+        if (!email) {
+            return NextResponse.redirect(new URL("/login", request.url))
+        }
+    }
+
+    if (url.pathname === "/reset-password") {
+        const token = url.searchParams.get("token");
+        if (!token) {
+            return NextResponse.redirect(new URL("/login", request.url));
+        }
+    }
+
     return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
