b3a8716f1f010f7c88ef0619aaa6d64a14faab8e
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-16 03:30:06 +0300

    Ajout d'un formulaire de vérification d'email avec gestion du code OTP. Intégration de la validation avec Zod et mise à jour de l'affichage des messages d'erreur.

--- app/(auth)/email-verified/page.tsx
+++ app/(auth)/email-verified/page.tsx
@@ -1,20 +1,37 @@
 "use client"
 
+import { z } from "zod"
 import { toast } from "sonner"
+import { useForm } from "react-hook-form"
 import { Button } from "@/src/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "@/src/components/ui/input-otp"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/src/components/ui/form"
 
+
+const FormSchema = z.object({
+    pin: z.string().min(6, {
+        message: "Le code de vérification doit contenir au moins 6 chiffres.",
+    }),
+})
 
 export default function EmailVerified() {
     /**
      * ! STATE (état, données) de l'application
      */
-
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            pin: "",
+        },
+    })
 
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-
+    const handleVerifyOtp = async (data: z.infer<typeof FormSchema>) => {
+        console.log(data)
+    }
 
     /**
      * ! AFFICHAGE (render) de l'application
@@ -28,40 +45,55 @@ export default function EmailVerified() {
                     Un code de vérification à 6 chiffres a été envoyé à votre adresse e-mail.
                     Veuillez entrer ce code ci-dessous pour confirmer votre compte.
                 </p>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleVerifyOtp)}>
+                        <div className="mb-4 flex justify-center">
+                            <FormField
+                                control={form.control}
+                                name="pin"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <InputOTP maxLength={6} {...field} autoFocus>
+                                                <InputOTPGroup>
+                                                    <InputOTPSlot index={0} className="bg-white font-inter" />
+                                                    <InputOTPSlot index={1} className="bg-white font-inter" />
+                                                    <InputOTPSlot index={2} className="bg-white font-inter" />
+                                                </InputOTPGroup>
+                                                <InputOTPSeparator />
+                                                <InputOTPGroup>
+                                                    <InputOTPSlot index={3} className="bg-white font-inter" />
+                                                    <InputOTPSlot index={4} className="bg-white font-inter" />
+                                                    <InputOTPSlot index={5} className="bg-white font-inter" />
+                                                </InputOTPGroup>
+                                            </InputOTP>
+                                        </FormControl>
+                                        <FormDescription>
+                                            Votre code de vérification à 6 chiffres <span> expirera dans 20 secondes.</span>
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
 
-                <form>
-                    <div className="mb-4 flex justify-center">
-                        <InputOTP maxLength={6} autoFocus>
-                            <InputOTPGroup>
-                                <InputOTPSlot index={0} className="bg-white font-inter" />
-                                <InputOTPSlot index={1} className="bg-white font-inter" />
-                                <InputOTPSlot index={2} className="bg-white font-inter" />
-                            </InputOTPGroup>
-                            <InputOTPSeparator />
-                            <InputOTPGroup>
-                                <InputOTPSlot index={3} className="bg-white font-inter" />
-                                <InputOTPSlot index={4} className="bg-white font-inter" />
-                                <InputOTPSlot index={5} className="bg-white font-inter" />
-                            </InputOTPGroup>
-                        </InputOTP>
-                    </div>
+                        <Button type="submit" className="w-full bg-blue-900 font-inter hover:bg-blue-950">
+                            Confirmer le code
+                        </Button>
 
-                    <Button type="submit" className="w-full bg-blue-900 font-inter hover:bg-blue-950">
-                        Confirmer le code
-                    </Button>
-
-                    {/* ✅ Message en cas de non-réception */}
-                    <p className="mt-4 font-inter text-sm text-gray-600">
-                        Vous n&apos;avez pas reçu le code ?{" "}
-                        <button
-                            type="button"
-                            className="font-spaceGrotesk text-blue-600 hover:underline"
-                            onClick={() => toast.info("Un nouveau code a été envoyé.")}
-                        >
-                            Renvoyer le code
-                        </button>
-                    </p>
-                </form>
+                        {/* ✅ Message en cas de non-réception */}
+                        <p className="mt-4 font-inter text-sm text-gray-600">
+                            Vous n&apos;avez pas reçu le code ?{" "}
+                            <button
+                                type="button"
+                                className="font-spaceGrotesk text-blue-600 hover:underline"
+                                onClick={() => toast.info("Un nouveau code a été envoyé.")}
+                            >
+                                Renvoyer le code
+                            </button>
+                        </p>
+                    </form>
+                </Form>
             </div>
         </>
     )
