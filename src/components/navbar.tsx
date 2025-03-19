4265da27377c56dbef20ed4b267438b319d85873
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-20 01:05:23 +0300

    Amélioration de la structure du composant Navbar : réorganisation du code et ajustement des indentations pour une meilleure lisibilité

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { Button } from './ui/button'
 import ThemeSwitcher from './theme-switcher'
-import { CodeXml, Contact, House, Info,  LogIn, User } from 'lucide-react'
+import { CodeXml, Contact, House, Info, LogIn, User } from 'lucide-react'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue, } from "./ui/select"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "./ui/dropdown-menu"
 
@@ -21,95 +21,95 @@ export default function Navbar() {
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <nav className="relative z-10 mx-auto mt-4 flex h-14 w-full max-w-5xl items-center justify-between rounded-full border bg-white dark:bg-zinc-950">
+        <nav className="relative  z-10 mx-auto mt-4 flex h-14 w-full max-w-5xl items-center justify-between rounded-full border bg-white dark:bg-zinc-950">
 
-        {/* Logo et Menu */}
-        <div className="ms-5 flex items-center">
-            <CodeXml />
-            <ul className='ms-2 flex space-x-1'>
-                <li>
-                    <Link href="/">
-                        <Button variant={'ghost'} className='flex items-center font-spaceGrotesk'>
-                            <House className="size-3" />
-                            <span>Accueil</span>
+            {/* Logo et Menu */}
+            <div className="ms-5 flex items-center">
+                <CodeXml />
+                <ul className='ms-2 flex space-x-1'>
+                    <li>
+                        <Link href="/">
+                            <Button variant={'ghost'} className='flex items-center font-spaceGrotesk'>
+                                <House className="size-3" />
+                                <span>Accueil</span>
+                            </Button>
+
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#">
+                            <Button variant={'ghost'} className='flex items-center font-spaceGrotesk'>
+                                <Info className="size-3" />
+                                <span>A propos de moi</span>
+                            </Button>
+
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#">
+                            <Button variant={'ghost'} className='flex items-center font-spaceGrotesk'>
+                                <Contact className="size-3" />
+                                <span>Contact</span>
+                            </Button>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button
+                            size={'icon'}
+                            variant={'secondary'}
+                            className="mr-5 rounded-full border text-gray-600 dark:border-gray-600 dark:text-gray-200"
+                        >
+                            <User />
                         </Button>
+                    </DropdownMenuTrigger>
 
-                    </Link>
-                </li>
-                <li>
-                    <Link href="#">
-                        <Button variant={'ghost'} className='flex items-center font-spaceGrotesk'>
-                            <Info className="size-3" />
-                            <span>A propos de moi</span>
-                        </Button>
-
-                    </Link>
-                </li>
-                <li>
-                    <Link href="#">
-                        <Button variant={'ghost'} className='flex items-center font-spaceGrotesk'>
-                            <Contact className="size-3" />
-                            <span>Contact</span>
-                        </Button>
-                    </Link>
-                </li>
-            </ul>
-        </div>
-
-        <div>
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button
-                        size={'icon'}
-                        variant={'secondary'}
-                        className="mr-5 rounded-full border text-gray-600 dark:border-gray-600 dark:text-gray-200"
-                    >
-                        <User />
-                    </Button>
-                </DropdownMenuTrigger>
-
-                <DropdownMenuContent className="mt-2 w-72">
-                    <DropdownMenuGroup className='space-y-3 p-2'>
-                        {/* Theme Section */}
-                        <div className="flex items-center justify-between">
-                            <span className='font-spaceGrotesk text-sm'>Thème</span>
-                            <ThemeSwitcher />
-                        </div>
-
-                        {/* Language Section */}
-                        <div className="flex items-center justify-between">
-                            <span className='font-spaceGrotesk text-sm'>Langue</span>
-                            <Select defaultValue="fr">
-                                <SelectTrigger className="h-7 w-[130px] font-spaceGrotesk">
-                                    <SelectValue />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectGroup className='font-spaceGrotesk'>
-                                        <SelectItem value="fr">🇫🇷 Français</SelectItem>
-                                        <SelectItem value="en">🇬🇧 English</SelectItem>
-                                        <SelectItem value="es">🇲🇬 Malagasy</SelectItem>
-                                    </SelectGroup>
-                                </SelectContent>
-                            </Select>
-                        </div>
-                    </DropdownMenuGroup>
-
-                    <DropdownMenuSeparator />
-
-                    {/* Login Section */}
-                    <Link href="/login">
-                        <DropdownMenuItem className="font-spaceGrotesk font-medium">
-                            <div className="flex items-center space-x-2">
-                                <LogIn className="size-4" />
-                                <span>Se connecter</span>
+                    <DropdownMenuContent className="mt-2 w-72">
+                        <DropdownMenuGroup className='space-y-3 p-2'>
+                            {/* Theme Section */}
+                            <div className="flex items-center justify-between">
+                                <span className='font-spaceGrotesk text-sm'>Thème</span>
+                                <ThemeSwitcher />
                             </div>
-                            <DropdownMenuShortcut>⌘L</DropdownMenuShortcut>
-                        </DropdownMenuItem>
-                    </Link>
-                </DropdownMenuContent>
-            </DropdownMenu>
 
-        </div>
-    </nav>
+                            {/* Language Section */}
+                            <div className="flex items-center justify-between">
+                                <span className='font-spaceGrotesk text-sm'>Langue</span>
+                                <Select defaultValue="fr">
+                                    <SelectTrigger className="h-7 w-[130px] font-spaceGrotesk">
+                                        <SelectValue />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectGroup className='font-spaceGrotesk'>
+                                            <SelectItem value="fr">🇫🇷 Français</SelectItem>
+                                            <SelectItem value="en">🇬🇧 English</SelectItem>
+                                            <SelectItem value="es">🇲🇬 Malagasy</SelectItem>
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                        </DropdownMenuGroup>
+
+                        <DropdownMenuSeparator />
+
+                        {/* Login Section */}
+                        <Link href="/login">
+                            <DropdownMenuItem className="font-spaceGrotesk font-medium">
+                                <div className="flex items-center space-x-2">
+                                    <LogIn className="size-4" />
+                                    <span>Se connecter</span>
+                                </div>
+                                <DropdownMenuShortcut>⌘L</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                        </Link>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+            </div>
+        </nav>
     )
 }
